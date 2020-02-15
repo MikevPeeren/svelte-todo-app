c8495d7183a7cf8354c41d449e1ede2d4b2fca4d
MikevPeeren <MikevPeeren@hotmail.com>
2020-02-15 16:30:17 +0100

    Added Todo functionality

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -1,19 +1,157 @@
 <script>
+    import TodoItem from './TodoItem.svelte';
+
+    let newTodoTitle = '';
+    let currentFilter = 'all';
+    let nextId = 4;
+
     let todos = [
         {
             id: 1,
-            title: 'My first todo',
-            completed: false
+            title: 'Learn Svelte',
+            completed: false,
         },
         {
             id: 2,
-            title: 'My second todo',
-            completed: false
+            title: 'Cleanup the house',
+            completed: false,
         },
         {
             id: 3,
-            title: 'My third todo',
-            completed: false
+            title: 'Get Groceries',
+            completed: false,
         },
     ];
+
+    // Creating a new Todo item and adding it to the array.
+    function addToDo(event) {
+        if (event.key === 'Enter') {
+            todos = [
+                ...todos,
+                {
+                    id: nextId,
+                    completed: false,
+                    title: newTodoTitle,
+                },
+            ];
+            nextId = nextId + 1;
+            newTodoTitle = '';
+        }
+    }
+
+    // $: marks a statement as reactive
+    // Reactive statements run immediately before the component updates, whenever the values that they depend on have changed.
+    $: todosRemaining = filteredTodos.filter(todo => !todo.completed).length;
+    $: filteredTodos =
+        currentFilter === 'all'
+            ? todos
+            : currentFilter === 'completed'
+            ? todos.filter(todo => todo.completed)
+            : todos.filter(todo => !todo.completed);
+
+    // Checkbox to select all Checkboxes
+    function checkAllTodos(event) {
+        todos.forEach(todo => (todo.completed = event.target.checked));
+        todos = todos;
+    }
+
+    // Updates which Todo's are being shown.
+    function updateFilter(newFilter) {
+        currentFilter = newFilter;
+    }
+
+    // Removes all completed tasks
+    function clearCompleted() {
+        todos = todos.filter(todo => !todo.completed);
+    }
+
+    // Received from the TodoItem Component
+    function handleDeleteTodo(event) {
+        todos = todos.filter(todo => todo.id !== event.detail.id);
+    }
+
+    // Received from the TodoItem Component
+    function handleToggleComplete(event) {
+        const todoIndex = todos.findIndex(todo => todo.id === event.detail.id);
+        const updatedTodo = { ...todos[todoIndex], completed: !todos[todoIndex].completed };
+        todos = [...todos.slice(0, todoIndex), updatedTodo, ...todos.slice(todoIndex + 1)];
+    }
 </script>
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 10px auto;
+    }
+    .todo-input {
+        width: 100%;
+        padding: 10px, 20px;
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+    .inner-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 16px;
+        border-top: 1px solid lightgrey;
+        padding-top: 15px;
+        margin-bottom: 13px;
+    }
+    .inner-container-input {
+        margin-right: 12px;
+    }
+    button {
+        font-size: 14px;
+        background-color: white;
+        appearance: none;
+        padding: 15px;
+    }
+    button:hover {
+        background: lightgreen;
+    }
+    button:focus {
+        outline: none;
+    }
+    .active {
+        background: lightgreen;
+    }
+</style>
+
+<div class="container">
+    <input
+        type="text"
+        class="todo-input"
+        placeholder="Insert todo item ..."
+        bind:value={newTodoTitle}
+        on:keydown={addToDo} />
+
+    {#each filteredTodos as todo}
+        <div class="todo-item">
+            <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} />
+        </div>
+    {/each}
+
+    <div class="inner-container">
+        <div>
+            <label>
+                <input class="inner-container-input" type="checkbox" on:change={checkAllTodos} />
+                Check All
+            </label>
+        </div>
+        <div>{todosRemaining} items left</div>
+    </div>
+
+    <div class="inner-container">
+        <div>
+            <button on:click={() => updateFilter('all')} class:active={currentFilter === 'all'}>All</button>
+            <button on:click={() => updateFilter('active')} class:active={currentFilter === 'active'}>Active</button>
+            <button on:click={() => updateFilter('completed')} class:active={currentFilter === 'completed'}>
+                Completed
+            </button>
+        </div>
+        <dir>
+            <button on:click={clearCompleted}>Clear Completed</button>
+        </dir>
+    </div>
+</div>
