4b16c40bc4927f8f013a9c49e19ef7d530dc2d49
MikevPeeren <MikevPeeren@hotmail.com>
2020-02-15 15:40:22 +0100

    Cleanup

---
+++ src/Todos.svelte
@@ -0,0 +1,19 @@
+<script>
+    let todos = [
+        {
+            id: 1,
+            title: 'My first todo',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'My second todo',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'My third todo',
+            completed: false
+        },
+    ];
+</script>
